a5f49a4ef95f705a968ea0c8cf36e14bc7cfbe76
hideki <hideki@inoue-kobo.com>
2017-05-06 17:38:17 +0900

    Google Analytics用にreactGAを導入しました。

--- bin/container-update.sh
+++ bin/container-update.sh
@@ -4,6 +4,8 @@ APP_ROOT=$HOME/Dropbox/workspace/aigaomoi-web
 
 cd $APP_ROOT
 
+yarn run webpack
+
 $(aws ecr get-login --region ap-northeast-1)
 docker build -t inoue-kobo.com/aigaomoi-web .
 docker tag inoue-kobo.com/aigaomoi-web:latest 489378379658.dkr.ecr.ap-northeast-1.amazonaws.com/inoue-kobo.com/aigaomoi-web:latest
